799fd8cfdea7c6dfeb6c529f7af5b19735129c38
joabbremer <joab.bremer@gmail.com>
2016-06-12 18:49:27 -0300

    Financiamento List

--- src/bean/FinanciamentoBean.java
+++ src/bean/FinanciamentoBean.java
@@ -1,20 +1,53 @@
 package bean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.faces.view.ViewScoped;
 
+import controller.ParcelaController;
+import controller.SimulacaoController;
 import model.ParcelaModel;
+import model.SimulacaoModel;
 
 @ManagedBean
 @SessionScoped
 public class FinanciamentoBean {
 	
-	public List<ParcelaModel> ListParcelas(){
+	private DataModel<ParcelaModel> parcDataModel = new  ListDataModel<ParcelaModel>();
+	
+	public void CalcularFianciamento(){
+		SimulacaoController simulacaoController = SimulacaoController.getControllerInstance();
+		simulacaoController.Simular();
+	}
+	
+	public DataModel<ParcelaModel> ListParcela(){
+		SimulacaoController simulacaoController = SimulacaoController.getControllerInstance();
+		SimulacaoModel simulacaoModel = simulacaoController.ResultSimule();
+		ParcelaModel parcelaModel = ParcelaModel.getInstance();	
+		List<ParcelaModel> parcela = new ArrayList<ParcelaModel>();
+		for (int i = 0; i < simulacaoModel.getQtParcelas(); i++) {
+			parcela.add(parcelaModel);
+			
+		}
 		
-		return null;
 		
+		
+		parcDataModel = new  ListDataModel<ParcelaModel>(parcela);		
+		return parcDataModel;
+		 
+	}
+
+	public DataModel<ParcelaModel> getParcDataModel() {
+		return parcDataModel;
+	}
+
+	public void setParcDataModel(DataModel<ParcelaModel> parcDataModel) {
+		this.parcDataModel = parcDataModel;
 	}
 	
 
--- src/bean/SimulacaoBean.java
+++ src/bean/SimulacaoBean.java
@@ -2,18 +2,25 @@ package bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.view.ViewScoped;
 
 import controller.SimulacaoController;
 import model.SimulacaoModel;
 
 @ManagedBean
-@SessionScoped
+@ViewScoped
 public class SimulacaoBean {
 	
 	SimulacaoController simulacaoController = new SimulacaoController();
 	
+	
 	public SimulacaoModel PrepareToSimule(){
 		return simulacaoController.PrepareToSimule();
+		
+	}
+	
+	public SimulacaoModel Simule(){
+		return simulacaoController.Simule();
 		
 	}
 	
